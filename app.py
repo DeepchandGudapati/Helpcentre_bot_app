08d6871a48b3b17cc0269e8f4452ee83d0b57985
Deepchand <41330037+DeepchandGudapati@users.noreply.github.com>
2023-07-25 17:41:13 +0100

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 import pandas as pd
 import streamlit as st
-import openpyxl
+import requests
+from bs4 import BeautifulSoup
 
 # Load your dataset
 data = pd.read_excel("bot.xlsx")
 
-st.title("Get Help center articles")
+st.title("Bot: Get Links by Description")
 
 # User input for description
 user_input = st.text_input("Enter description:")
 
 # Find the link corresponding to the description
 if user_input:
-    link = data[data["description"].str.contains(user_input, case=False)]["link"].iloc[0]
-    if link:
+    filtered_data = data[data["description"].str.contains(user_input, case=False)]
+
+    if not filtered_data.empty:
+        link = filtered_data["link"].iloc[0]
         st.success(f"Link: {link}")
+
+        # Fetch webpage content and extract a preview
+        try:
+            response = requests.get(link)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, "html.parser")
+                preview_text = soup.get_text()
+                st.write("Preview:")
+                st.text(preview_text)
+            else:
+                st.warning("Failed to fetch webpage content.")
+        except requests.exceptions.RequestException as e:
+            st.warning(f"Error: {e}")
     else:
         st.warning("No link found for the provided description.")
