ed442f994346944bd72d04373621e591c8d3a6bd
Deepchand <41330037+DeepchandGudapati@users.noreply.github.com>
2023-07-25 17:14:21 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import streamlit as st
+
+# Load your dataset
+data = pd.read_excel("bot.xlsx")
+
+st.title("Bot: Get Links by Description")
+
+# User input for description
+user_input = st.text_input("Enter description:")
+
+# Find the link corresponding to the description
+if user_input:
+    link = data[data["description"].str.contains(user_input, case=False)]["link"].iloc[0]
+    if link:
+        st.success(f"Link: {link}")
+    else:
+        st.warning("No link found for the provided description.")
