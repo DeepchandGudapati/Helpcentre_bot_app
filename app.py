736c33c423866da18b159822b7894ee909ac5657
Deepchand <41330037+DeepchandGudapati@users.noreply.github.com>
2023-07-28 19:18:05 +0100

    app.py

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
 from fuzzywuzzy import process
-import json
-
+from IPython.display import display, Image
 
 # Load your dataset
 data = pd.read_excel("bot.xlsx")
 
-# GIF URL or local file path
-gif_url = "original-4591cc3d8ca4a9f6cbe8081f7c6d16e0.gif"
+# GIF URL from GitHub
+gif_url = "original-4591cc3d8ca4a9f6cbe8081f7c6d16e0.gif"  # Replace with the raw URL of your GIF hosted on GitHub
 
 # CSS styling to create splash screen and zoom-in effect
 st.markdown(
@@ -121,31 +118,38 @@ with st.container():
         unsafe_allow_html=True,
     )
 
-st.title("Get help center Links")
+    # Display the main content of your Streamlit app here
 
-# User input for description
-user_input = st.text_input("Enter description:")
+    st.title("Get help center Links")
 
-# Search suggestions based on the available descriptions
-if user_input:
-    suggestions = process.extract(user_input, data["description"], limit=5)
-    suggestions = [suggestion[0] for suggestion in suggestions if suggestion[1] >= 80]
-    st.write("Search Suggestions:")
-    # Display suggestions as hyperlinks
-    for suggestion in suggestions:
-        link = data.loc[data["description"] == suggestion, "link"].iloc[0]
-        st.markdown(f"[{suggestion}]({link})", unsafe_allow_html=True)
+    # User input for description
+    user_input = st.text_input("Enter description:")
 
-# Find the link corresponding to the description
-if user_input:
-    filtered_data = data[data["description"].str.contains(user_input, case=False)]
+    # Find the link corresponding to the description
+    if user_input:
+        filtered_data = data[data["description"].str.contains(user_input, case=False)]
 
-    if not filtered_data.empty:
-        link = filtered_data["link"].iloc[0]
-        st.success(f"Link: {link}")
+        if not filtered_data.empty:
+            link = filtered_data["link"].iloc[0]
+            st.success(f"Link: {link}")
 
-        # Fetch webpage content and extract a preview (same as previous implementation)
-        # ...
+            # Fetch webpage content and extract a preview (same as previous implementation)
+            # ...
 
-    else:
-        st.warning("No link found for the provided description.")
+        else:
+            st.warning("No link found for the provided description.")
+
+    # Create an empty space to move suggestions below the link field
+    suggestions_placeholder = st.empty()
+
+    # Search suggestions based on the available descriptions
+    if user_input:
+        suggestions = process.extract(user_input, data["description"], limit=5)
+        suggestions = [suggestion[0] for suggestion in suggestions if suggestion[1] >= 80]
+        suggestions_placeholder.write("Search Suggestions:")
+        # Display suggestions as hyperlinks
+        for suggestion in suggestions:
+            link = data.loc[data["description"] == suggestion, "link"].iloc[0]
+            suggestions_placeholder.markdown(f"[{suggestion}]({link})", unsafe_allow_html=True)
+
+    st.markdown("</div>", unsafe_allow_html=True)
