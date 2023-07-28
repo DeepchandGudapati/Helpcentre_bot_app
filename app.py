7700fadfc4487e8fac565bc5b2d23fa2fa1fc9f4
Deepchand <41330037+DeepchandGudapati@users.noreply.github.com>
2023-07-28 19:45:43 +0100

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,79 @@
 import pandas as pd
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
 from fuzzywuzzy import process
-import json
 
 # Load your dataset
 data = pd.read_excel("bot.xlsx")
 
+# CSS styling for Meta theme
+st.markdown(
+    """
+    <style>
+    /* Container width and alignment */
+    .stApp {
+        max-width: 960px; /* Adjust to your preference */
+        margin: 0 auto;
+    }
+
+    /* Main container background and border radius */
+    .stApp div[data-testid="stDecoration"] {
+        background-color: #f5f6f8;
+        border-radius: 12px; /* Adjust to your preference */
+        padding: 24px; /* Adjust to your preference */
+    }
+
+    /* Header background color */
+    .stApp div[data-testid="stSessionInfo"] {
+        background-color: #1877f2; /* Meta blue */
+    }
+
+    /* Header text color */
+    .stApp div[data-testid="stSessionInfo"] span {
+        color: #ffffff; /* White text */
+    }
+
+    /* Widget styles */
+    .stButton {
+        background-color: #1877f2; /* Meta blue */
+        color: #ffffff; /* White text */
+        border-radius: 12px; /* Rounded corners */
+        padding: 12px 24px; /* Adjust to your preference */
+    }
+
+    .stTextInput {
+        border-radius: 12px; /* Rounded corners */
+        background-color: #ffffff; /* White background */
+    }
+
+    /* Suggestions styling */
+    .suggestions-container {
+        background-color: #ffffff;
+        border: 1px solid #dddddd;
+        border-radius: 12px;
+        padding: 12px;
+    }
+
+    .suggestion-link {
+        display: block;
+        margin-bottom: 8px;
+    }
+
+    .suggestion-link a {
+        color: #1877f2; /* Meta blue */
+        text-decoration: none;
+        font-weight: bold;
+    }
+
+    .suggestion-link a:hover {
+        text-decoration: underline;
+    }
+
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Main app
 st.title("Get help center Links")
 
 # User input for description
@@ -17,11 +83,12 @@ user_input = st.text_input("Enter description:")
 if user_input:
     suggestions = process.extract(user_input, data["description"], limit=5)
     suggestions = [suggestion[0] for suggestion in suggestions if suggestion[1] >= 80]
+    st.markdown("<div class='suggestions-container'>", unsafe_allow_html=True)
     st.write("Search Suggestions:")
-    # Display suggestions as hyperlinks
     for suggestion in suggestions:
         link = data.loc[data["description"] == suggestion, "link"].iloc[0]
-        st.markdown(f"[{suggestion}]({link})", unsafe_allow_html=True)
+        st.markdown(f"<div class='suggestion-link'><a href='{link}'>{suggestion}</a></div>", unsafe_allow_html=True)
+    st.markdown("</div>", unsafe_allow_html=True)
 
 # Find the link corresponding to the description
 if user_input:
