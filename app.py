b78000b8e9a80b05b60b8aab5e8f396b9aeb4345
Deepchand <41330037+DeepchandGudapati@users.noreply.github.com>
2023-07-28 19:35:41 +0100

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import pandas as pd
 import streamlit as st
+import requests
+from bs4 import BeautifulSoup
 from fuzzywuzzy import process
+import json
 
 # Load your dataset
 data = pd.read_excel("bot.xlsx")
 
-# Main app
 st.title("Get help center Links")
 
 # User input for description
 user_input = st.text_input("Enter description:")
+
+# Search suggestions based on the available descriptions
+if user_input:
+    suggestions = process.extract(user_input, data["description"], limit=5)
+    suggestions = [suggestion[0] for suggestion in suggestions if suggestion[1] >= 80]
+    st.write("Search Suggestions:")
+    # Display suggestions as hyperlinks
+    for suggestion in suggestions:
+        link = data.loc[data["description"] == suggestion, "link"].iloc[0]
+        st.markdown(f"[{suggestion}]({link})", unsafe_allow_html=True)
 
 # Find the link corresponding to the description
 if user_input:
@@ -24,17 +36,3 @@ if user_input:
 
     else:
         st.warning("No link found for the provided description.")
-
-# Create an empty space to move suggestions below the link field
-suggestions_placeholder = st.empty()
-
-# Search suggestions based on the available descriptions
-if user_input:
-    choices = data["description"].tolist()  # Convert the description column to a list
-    suggestions = process.extract(user_input, choices, limit=len(data))
-    suggestions = [suggestion[0] for suggestion in suggestions if suggestion[1] >= 80]
-    suggestions_placeholder.write("Search Suggestions:")
-    # Display suggestions as hyperlinks
-    for suggestion in suggestions:
-        link = data.loc[data["description"] == suggestion, "link"].iloc[0]
-        suggestions_placeholder.markdown(f"[{suggestion}]({link})", unsafe_allow_html=True)
